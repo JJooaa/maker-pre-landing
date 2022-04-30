6f9383021bd151701cd05065f6bc69849633d918
JJooaa <leitola.joa@gmail.com>
2022-04-30 19:46:27 +0300

    save changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cyan: "#3EE9E5",
+        blue: "#09F68",
+        gray: "#777F96",
+        white: "FFFFFF",
+        black: "#080C20",
+        red: "#FF2965",
+      },
+      fontFamily: {
+        manrope: ["Manrope", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
